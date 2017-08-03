3f0e189f7a8824f67bc600cf6b8ffa3978a1ee7f
montyD <billy-da-fish-3rd@hotmail.com>
2017-08-03 20:50:55 +0100

    Add podcast info to player component

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -10,11 +10,9 @@ interface Props {
   playingPodcast: PlayingPodcast;
 }
 
-const mapStateToProps = (state: State) => {
-  return {
+const mapStateToProps = (state: State) => ({
   playingPodcast: state.playingPodcast
-}
-};
+});
 
 const mapDispatchToProps = (dispatch: Function) => ({
   onPause: () => dispatch({ type: PAUSE_PODCAST }),
@@ -26,6 +24,10 @@ class Player extends React.Component<Props, {}> {
   private audioElement: HTMLAudioElement;
   private plyrInstance: plyr.Plyr;
 
+  contructor() {
+    this.podcastInformation = this.podcastInformation.bind(this);
+  }
+
   componentDidMount() {
     this.plyrInstance = plyr.setup(this.audioElement, {})[0];
     this.plyrInstance.on('pause', this.props.onPause.bind(this));
@@ -36,7 +38,7 @@ class Player extends React.Component<Props, {}> {
     });
   }
 
-  shouldComponentUpdate(newProps: Props) {
+  componentWillUpdate(newProps: Props) {
     let state = newProps.playingPodcast;
     if (state.podcast && this.plyrInstance.source().indexOf(state.podcast.url) === -1) {
       this.plyrInstance.source({
@@ -53,12 +55,24 @@ class Player extends React.Component<Props, {}> {
     } else {
       this.plyrInstance.pause();
     }
-    return false; // never rerender
+  }
+
+  podcastInformation() {
+    let podcast = this.props.playingPodcast.podcast;
+    if (!podcast) {
+      return null;
+    }
+    return (
+      <div>
+        <strong>{podcast.title}</strong>
+      </div>
+    );
   }
 
   render() {
     return (
       <div>
+        {this.podcastInformation()}
         <audio ref={audioEl => this.audioElement = audioEl!} />
       </div>
     );
